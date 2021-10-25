f37a2f9c1bde9707c4b9c43bd13e4097aa8d5092
iTIC <paul2314@naver.com>
2021-10-25 23:25:55 +0900

    fix(randomReply): changed randomReply function logic

--- src/react-functions/index.ts
+++ src/react-functions/index.ts
@@ -8,14 +8,23 @@ export function staticReply(replyText: string): ReactFunction {
 	}
 }
 
-export function todayRandomReply(...args: string[]): ReactFunction {
+export function randomReply(
+	args: string[],
+	option?: { seed: number },
+): ReactFunction {
 	return (message: Message) => {
-		const today = new Date()
-		const todayNum =
-			today.getDate().toString() +
-			today.getMonth().toString() +
-			today.getFullYear().toString()
-		const replytext = arguments[parseInt(todayNum) % arguments.length]
-		message.reply(replytext)
+		if (option?.seed) {
+			const today = new Date(option.seed)
+			const todayNum =
+				today.getDate().toString() +
+				today.getMonth().toString() +
+				today.getFullYear().toString()
+			const replytext = args[parseInt(todayNum) % args.length]
+			message.reply(replytext)
+		} else {
+			const randomNum = Math.floor(Math.random() * args.length)
+			const replytext = args[randomNum]
+			message.reply(replytext)
+		}
 	}
 }
