0a3c916cae9cf52f28ffc6123bf9cac07afc3d74
whitekiwi <jh145478@gmail.com>
2021-10-25 19:45:39 +0900

    temp

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,10 @@ client.on('messageCreate', (msg) => {
 	if (msg.content?.toUpperCase() === '창민 물줘') {
 		msg.reply('아옳옳옳!!')
 	}
+	if (msg.content?.toUpperCase() === 'PING') {
+		msg.reply('Pong!')
+	}
+	if (msg.content?.toUpperCase()?.startsWith('게임 추천')) {
+		msg.reply('APEX 해, APEX 갓 겜, 하지만 난 못 해, 너무 슬 퍼')
+	}
 })
