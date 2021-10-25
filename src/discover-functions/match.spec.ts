f230beda4b26e71fea288d74dd5b3e4ed5e82ac7
whitekiwi <jh145478@gmail.com>
2021-10-25 22:34:15 +0900

    test(): Add TC for match function

---
+++ src/discover-functions/match.spec.ts
@@ -0,0 +1,32 @@
+import { Message } from '../types'
+import { match } from '.'
+
+describe('match', () => {
+	it('성공 케이스', async () => {
+		const keyword = '창민'
+		const text = '창민'
+		expect(match(keyword)({ content: text } as Message)).toBeTruthy()
+	})
+
+	it('성공 케이스 - 공백제거 옵션', async () => {
+		const keyword = '창 민'
+		const text = '창민'
+		expect(
+			match(keyword, { ignoreSpace: true })({ content: text } as Message),
+		).toBeTruthy()
+	})
+
+	it('실패 케이스', async () => {
+		const keyword = '창민'
+		const text = '창민2'
+		expect(match(keyword)({ content: text } as Message)).toBeFalsy()
+	})
+
+	it('실패 케이스 - 공백제거 옵션', async () => {
+		const keyword = '창민'
+		const text = '창민2'
+		expect(
+			match(keyword, { ignoreSpace: true })({ content: text } as Message),
+		).toBeFalsy()
+	})
+})
