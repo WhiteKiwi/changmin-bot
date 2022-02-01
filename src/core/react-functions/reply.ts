cd2ab80d41b28f947304b3c76956a1352717c7d0
iTIC <paul2314@naver.com>
2022-02-02 00:41:31 +0900

    fix(changmin-lang): 코드 정리

--- src/core/react-functions/reply.ts
+++ src/core/react-functions/reply.ts
@@ -2,9 +2,14 @@ import { ReactFunction } from '../../common/types'
 import { client } from '../db'
 import { react } from './react'
 
-export function reply(replyText: string | (() => string)): ReactFunction {
-	return react(async ({ message, content }) => {
-		const text = typeof replyText === 'string' ? replyText : replyText()
+export function reply(
+	replyText: string | ((message: string) => string),
+): ReactFunction {
+	return react(async ({ message }) => {
+		const text =
+			typeof replyText === 'string'
+				? replyText
+				: replyText(message.content)
 		await message.reply(text)
 		await client.insert('replyLogs', { Original: content, Reply: text })
 	})
--- src/interactions/changmin-lang.ts
+++ src/interactions/changmin-lang.ts
@@ -1,12 +1,9 @@
 import { Interaction } from '../common'
-import { runChangminLangInterpreter } from '../common/lib'
+import { runChangminLang } from '../common/lib'
 import { startWith } from '../core/discover-functions'
-import { react } from '../core/react-functions'
+import { reply } from '../core/react-functions'
 
 export const changminLang: Interaction = {
 	discoverFunction: startWith('창민그거해봐그거', { ignoreSpace: true }),
-	reactFunction: react(async ({ message }) => {
-		const text = runChangminLangInterpreter(message.content)
-		await message.reply(text)
-	}),
+	reactFunction: reply(runChangminLang),
 }
